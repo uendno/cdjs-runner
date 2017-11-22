a5bf2e9f7e9b5d779f8a0245df6345b3e4206762
Tran Viet Thang <thangtv@foobla.com>
2017-11-22 21:40:24 +0700

    fix stages

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
-    git: require('./utils/git'),
-    shell: require('./utils/shell'),
-    wait: require('./utils/wait'),
-    cwd: require('./utils/cwd'),
-    stage: require('./utils/stage'),
-    stages:require('./utils/stages'),
-    steps: require('./utils/steps'),
+    git: require('./lib/utils/git'),
+    shell: require('./lib/utils/shell'),
+    wait: require('./lib/utils/wait'),
+    cwd: require('./lib/utils/cwd'),
+    stage: require('./lib/utils/stage'),
+    stages:require('./lib/utils/stages'),
+    steps: require('./lib/utils/steps'),
     exec: (fn) => fn(),
 
     DEFAULT_CWD: null
